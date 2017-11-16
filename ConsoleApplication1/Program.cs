8d7042387950b8d4358ef7566aed98f58508a7ce
wangrunmin <272295451@qq.com>
2017-11-16 10:34:05 +0800

    添加项目文件。

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = null;
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ MVCTEST/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace MVCTEST.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public string Index()
+        {
+            XmlDocument doc = new XmlDocument();
+            //var bpmResponse = WebServiceHelper.sendSupplierAccess(Request.Url.Authority, currentSupplierEnt);
+            doc.LoadXml(@"<Response>
+  <Status>T</Status> 
+<BpmID>100</BpmID>
+  <REMSG>同步成功</REMSG>
+</Response>");
+            XmlNode root = doc.SelectSingleNode("Response");
+
+            return root.SelectSingleNode("Status").InnerText;
+        }
+    }
+}
